dbe7db2bf6b3d5eeaaa2f2aeb0d153947ca9e6ce
zachyutw1117 <zachyu.tw@gmail.com>
2019-09-25 23:22:42 -0700

    Updates

---
+++ precache-manifest.eb3406ebfce4b880d627b6ad72566c89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cb7e93fded8902b85ee2983da6e89a",
+    "url": "/react-use-countdown-timer/index.html"
+  },
+  {
+    "revision": "3a9272642cb7ab103328",
+    "url": "/react-use-countdown-timer/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "3645642515b938c7e92e",
+    "url": "/react-use-countdown-timer/static/js/2.75fd940c.chunk.js"
+  },
+  {
+    "revision": "3a9272642cb7ab103328",
+    "url": "/react-use-countdown-timer/static/js/main.99f84b3d.chunk.js"
+  },
+  {
+    "revision": "de1a25faa34a8470d4da",
+    "url": "/react-use-countdown-timer/static/js/runtime-main.4fe0dd7e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-use-countdown-timer/precache-manifest.eb3406ebfce4b880d627b6ad72566c89.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-use-countdown-timer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
